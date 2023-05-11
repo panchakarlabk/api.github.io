d2b0c29f54ca9382e28976ad22f1c8eb31ada0d6
panchakarlabk <bharat_icom@icomsol.co.uk>
2023-05-11 10:13:12 +0100

    init

---
+++ api.github.io/Controllers/BooksController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api.github.io.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        // GET: api/<BooksController>
+        [HttpGet(Name = "books")]
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Book1", "Book2" };
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Book1";
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
