67105b70fb8723450f077a55a0783c73a68a16ef
panchakarlabk <82812710+panchakarlabk@users.noreply.github.com>
2023-05-22 15:45:54 +0100

    Update WeatherForecastController.cs

--- api.github.io/Controllers/WeatherForecastController.cs
+++ api.github.io/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api.github.io.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm ", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
